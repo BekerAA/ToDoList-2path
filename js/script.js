c2486e7bfbc97d6f7a78a329f93089bcaa81f737
Andrey Pushkov <andrey.pushkov.99@bk.ru>
2024-03-10 12:16:19 +0300

    create about and team

---
+++ js/script.js
@@ -0,0 +1,21 @@
+const dom = {
+  new: document.getElementById("new--task"),
+  add: document.getElementById("add"),
+  tasks: document.getElementById("tasks")
+}
+
+const tasks = [] 
+
+
+
+dom.add.onclick = () =>{
+  const TaskNewText = dom.new.value;
+  if (TaskNewText){
+
+  }
+}
+
+//функция для добавления задачи
+
+
+function addTasks()
